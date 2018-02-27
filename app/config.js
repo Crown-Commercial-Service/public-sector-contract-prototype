39ffed82507ea5119af58d8752bd9d609c4287eb
David McDonald <idavidmcdonald@me.com>
2018-02-27 14:01:42 +0000

    Set up index page and v1 prototype folder

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Public Sector Contract',
 
   // Default port that prototype runs on
   port: '3000',
