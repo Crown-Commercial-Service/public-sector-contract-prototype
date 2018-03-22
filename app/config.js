63cd974b73d23e38bdbca3a8ead3dc52241ee7bd
tracy-content <tracy.hughes@digital.cabinet-office.gov.uk>
2018-03-22 11:27:54 +0000

    Updated title config.js

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Build a public sector contract',
+  serviceName: 'Create a contract',
 
   // Default port that prototype runs on
   port: '3000',
