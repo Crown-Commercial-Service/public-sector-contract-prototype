32f6ce06eb99d109ccb7920ed9ce135d6d64c246
David McDonald <idavidmcdonald@me.com>
2018-03-22 11:42:16 +0000

    Move schedules into routes

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,119 @@
 var express = require('express')
 var router = express.Router()
 
+schedules = [
+	{
+		"name": 'Framework schedule 1: Specification',
+		"optional": false
+	},
+	{
+		"name": 'Framework schedule 2: Framework tender',
+		"optional": false
+	},
+	{
+		"name": 'Framework schedule 3: Framework prices',
+		"optional": false
+	},
+	{
+		"name": 'Framework schedule 4: Framework management',
+		"optional": false
+	},
+	{
+		"name": 'Framework schedule 5: Management charges and information',
+		"optional": false
+	},
+	{
+		"name": 'Framework schedule 6: Call-off award procedure',
+		"optional": false
+	},
+	{
+		"name": 'Framework schedule 7: Self audit certificate',
+		"optional": false
+	},
+	{
+		"name": 'Joint schedule 1: Definitions',
+		"optional": false
+	},
+	{
+		"name": 'Joint schedule 2: Variation form',
+		"optional": false
+	},
+	{
+		"name": 'Joint schedule 3: Insurance requirements',
+		"optional": false
+	},
+	{
+		"name": 'Joint schedule 4: Commercially sensitive information',
+		"optional": false
+	},
+	{
+		"name": 'Joint schedule 5: Corporate social responsibility',
+		"optional": false
+	},
+	{
+		"name": 'Joint schedule 6: Rectification plan',
+		"optional": false
+	},
+	{
+		"name": 'Joint schedule 7: Processing data',
+		"optional": false
+	},
+	{
+		"name": 'Joint schedule 8: Definitions',
+		"optional": false
+	},
+	{
+		"name": 'Call-off schedule 1: Transparency reports',
+		"optional": false
+	},
+	{
+		"name": 'Call-off schedule 2: Staff transfer',
+		"optional": false
+	},
+	{
+		"name": 'Call-off schedule 3: Continuous improvement',
+		"optional": false
+	},
+	{
+		"name": 'Call-off schedule 4: Exit management',
+		"optional": false
+	},
+	{
+		"name": "Framework schedule 9: Cyber essentials scheme",
+		"optional": true
+	},
+	{
+		"name": "Call-off schedule 4: Call-off tender",
+		"optional": true
+	},
+	{
+		"name": "Call-off schedule 6: ICT services",
+		"optional": true
+	},
+	{
+		"name": "Call-off schedule 10: Exit management",
+		"optional": true
+	},
+	{
+		"name": "Call-off schedule 11: Installation works",
+		"optional": true
+	},
+	{
+		"name": "Call-off schedule 12: Background checks",
+		"optional": true
+	}
+]
+
 // Route index page
 router.get('/', function (req, res) {
   res.render('index')
 })
 
 // add your routes here
+
+router.get('/v2/schedule/:scheduleId', function (req, res) {q
+scheduleId = req.params["scheduleId"] - 1
+  res.render('v2/schedule', {schedule: schedules[scheduleId]})
+})
 
 module.exports = router
